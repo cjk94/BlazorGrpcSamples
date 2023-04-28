ab51a8a6e448ae49eeb337f9ea3a384c75a0fc40
Connor Kalina <cjk94@pitt.edu>
2023-04-28 16:13:25 -0400

    try 2

--- Standalone/BlazorGrpcStandalone/Startup.cs
+++ Standalone/BlazorGrpcStandalone/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using BlazorGrpc;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
@@ -16,7 +17,10 @@ namespace BlazorGrpcStandalone
                 // Create a gRPC-Web channel pointing to the backend server
                 var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
 
-                var channel = GrpcChannel.ForAddress("https://radiology-devcon.bayer.com", new GrpcChannelOptions { HttpClient = httpClient });
+                //potentially needed to call insecure grpc services with .net core 
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+                var channel = GrpcChannel.ForAddress("https://radiology-devcon.bayer.com:5001", new GrpcChannelOptions { HttpClient = httpClient });
 
                 // Now we can instantiate gRPC clients for this channel
                 return new WeatherForecasts.WeatherForecastsClient(channel);
